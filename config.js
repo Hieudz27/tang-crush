1163fdc7d4951b94a6c98f2821d9167d0be48681
Hieudz27 <99593888+Hieudz27@users.noreply.github.com>
2022-02-13 19:15:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Bé à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnIntro: 'Hì😘',
+    title: ' Tóc Em Màu Đen,Môi Em Màu Đỏ 😘 ',
+    desc: ' Lại Gần Anh Hỏi Nhỏ,Em Có Yêu Anh Ko 🙆‍♂️ ',
+    btnYes: 'Dạ Có Yêu Anh <33',
+    btnNo: 'Không,Em Đéo Yêu Anh :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Vì Anh Mang Lại Cho Em Hạnh Phúc Và Vui Vẻ Lúc Bên Anh',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messDesc: 'Chúc Em 1 Ngày Valentin Vui Vẻ Và Càng Càng Yêu Anh   😘 .',
+    btnAccept: 'Dạ Yêu Anh <3',
+    messLink: 'https://ngytvn.net/index.php?id=wVchcAgZgAV 
 }
